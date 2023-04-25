57a1b50be9282d9c1721ae226394b3ceb2ca6b3e
Sindrefr <90753422+Sindrefr@users.noreply.github.com>
2023-04-25 22:27:43 +0200

    jaja

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const app = express();
 require('dotenv').config()
+const session = require('express-session');
+const { ObjectId } = require('mongodb');
+
+
+
+app.use(session({
+  secret: 'your-secret-key',
+  resave: false,
+  saveUninitialized: true
+}));
+
 
 // Serve static files
 app.use(express.static('public'));
@@ -48,6 +59,16 @@ app.get('/admin', (req, res) => {
   res.render('admin');
 });
 
+function requireAuth(req, res, next) {
+  if (req.session && req.session.user) {
+    // If the user is authenticated, call the next middleware function
+    return next();
+  } else {
+    // If the user is not authenticated, redirect to the login page
+    res.redirect('/admin');
+  }
+}
+
 app.post('/admin', (req, res) => {
   const { username, password } = req.body;
 
@@ -56,21 +77,49 @@ app.post('/admin', (req, res) => {
       console.log(err);
     } else {
       if (user) {
-        // If the username and password match, redirect to the add-products page
+        // If the username and password match, set the user in the session and redirect to the add-products page
+        req.session.user = user;
         res.redirect('/add-products');
       } else {
         // If the username and password don't match, display an error message
         res.render('admin', { errorMessage: 'Invalid username or password!' });
-
       }
     }
   });
 });
 
+
 // Render the add-products page
-app.get('/add-products', (req, res) => {
-  res.render('add-products');
+app.get('/add-products', requireAuth, (req, res) => {
+  productsCollection.find().toArray((err, products) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('add-products', { products: products });
+    }
+  });
 });
+
+
+
+
+app.get('/veileder', requireAuth, (req, res) => {
+  res.render('veileder');
+});
+
+
+app.post('/delete-product', requireAuth, (req, res) => {
+  const productId = req.query.id;
+  productsCollection.deleteOne({ _id: ObjectId(productId) }, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Product deleted from database');
+      res.redirect('/add-products');
+    }
+  });
+});
+
 
 // Add a new product to the database
 app.post('/products', (req, res) => {
